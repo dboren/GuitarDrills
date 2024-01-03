bde916591f8610bf038a37eda187e5eef620cad5
Daniel Boren <danielboren52@gmail.com>
2024-01-02 21:36:28 -0600

    added js file with E major pentatonic scale notes defined according to fretboard coordinates.

---
+++ testPitch.js
@@ -0,0 +1,25 @@
+//Below are the notes of an E major pentatonic scale defined according to pitch class name and their string/fret positions on the fretboard. Can I program this so that it will render the tab in the correct positions?
+
+const E2 = {
+    "coords" : [[0, 0]]
+}
+
+const Fs2 = {
+    "coords" : [[0, 2]]
+}
+
+const Gs2 = {
+    "coords" : [[0, 4]]
+}
+
+const B2 = {
+    "coords" : [[0, 7], [1, 2]]
+}
+
+const Cs3 = {
+    "coords" : [[0, 9], [1, 4]]
+}
+
+const E3 = {
+    "coords" : [[0, 12], [1, 7], [1, 19], [2, 2]]
+}
